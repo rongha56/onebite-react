a86f3a6d64754f55420463e7d07a5f2198c861e6
rongha56 <rongha56@gmail.com>
2025-04-26 10:18:13 +0900

    chapter07

---
+++ section01/chapter07.js
@@ -0,0 +1,28 @@
+//1. 대입 연산자
+let var1 = 1;
+
+//2. 산술 연산자
+let num1 = 3 + 2;
+let num2 = 3 - 2;
+let num3 = 3 * 2;
+let num4 = 3 / 2;
+let num5 = 3 % 2;
+
+let num6 = 1 + 2 * 10; //21
+let num7 = (1 + 2) * 10; //30
+console.log(num6);
+console.log(num7);
+
+//3. 복합 대입 연산자
+let num8 = 10;
+num8 += 20;
+num8 -= 20;
+num8 *= 20;
+num8 /= 20;
+num8 %= 20;
+console.log(num8);
+
+//4. 증감 연산자
+let num9 = 10;
+num9++;
+console.log(num9);
